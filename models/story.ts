bea8220cc16ecaa92aee998e5c7ef8ef1a0fc444
prernabarodiya <prernabarodiya@gmail.com>
2025-07-17 17:10:00 +0530

    initial commit

---
+++ models/story.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+export interface IStory {
+    authorId: mongoose.Types.ObjectId;
+    _id?: mongoose.Types.ObjectId;
+    location: string;
+    images: string[];
+    details: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const storySchema = new Schema<IStory>(
+    {
+        authorId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",  // Reference the User model
+            required: true
+        },
+        location: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: [String],
+            required: true
+        },
+        details: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Story = models?.Story || model<IStory>("Story", storySchema);
+export default Story;
